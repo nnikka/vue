66f8c2bdc99d80f94c2d0fe0956e5d79808eddb2
nika <nikakhurashvili@gmail.com>
2019-02-01 12:59:27 +0400

    add console notifier

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -17,6 +17,7 @@ export const createCompiler = createCompilerCreator(function baseCompile (
     optimize(ast, options)
   }
   const code = generate(ast, options)
+  console.log("asdasd")
   return {
     ast,
     render: code.render,
